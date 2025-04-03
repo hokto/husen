b33104a870aa3b7cb8254117c53350a3a8efc689
sekine-1113 <sekine104@outlook.jp>
2025-04-03 22:27:40 +0900

    apiの実装

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,13 +1,23 @@
-import express, { Request, Response } from 'express';
+import express, { NextFunction, Request, Response } from 'express';
+import cors from 'cors';
+import router from './sticky-notes';
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
+app.use(cors());
+app.use((req: Request, res: Response, next: NextFunction) => {
+  console.log(req.method, req.url);
+  next();
+});
+app.use('/api/v1/sticky-notes', router);
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello, TypeScript with Express!');
 });
+
+
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
