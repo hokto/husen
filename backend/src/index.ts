b4b8cb05ea694f64bf962acbdb086b56838c3c4d
sekine-1113 <sekine104@outlook.jp>
2025-04-03 22:28:54 +0900

    リファクタ

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -7,17 +7,17 @@ const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(cors());
+
 app.use((req: Request, res: Response, next: NextFunction) => {
   console.log(req.method, req.url);
   next();
 });
+
 app.use('/api/v1/sticky-notes', router);
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello, TypeScript with Express!');
 });
-
-
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
