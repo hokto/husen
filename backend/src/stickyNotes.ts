f435e5d55e634f51068d05d21d4c50c1438bbdd2
sekine-1113 <sekine104@outlook.jp>
2025-04-09 00:14:48 +0900

    composeの修正

--- backend/src/stickyNotes.ts
+++ backend/src/stickyNotes.ts
@@ -1,4 +1,5 @@
 import { Router, Request, Response } from "express";
+import { StatusInfo } from "./status";
 
 
 const router = Router()
@@ -8,15 +9,15 @@ router.get('/', (req: Request, res: Response) => {
 });
 
 router.post('/', (req: Request, res: Response) => {
-    res.status(201).send();
+    res.status(StatusInfo.Created.statusCode).send();
 });
 
 router.put('/:id', (req: Request, res: Response) => {
-    res.status(204).send();
+    res.status(StatusInfo.NoContent.statusCode).send();
 });
 
 router.delete('/:id', (req: Request, res: Response) => {
-    res.status(204).send();
+    res.status(StatusInfo.NoContent.statusCode).send();
 });
 
 export default router;
