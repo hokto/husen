f5d0eba26f45fa4ca82b1dc823e718730b744c93
hokto <hokuto.k1441@gmail.com>
2025-04-09 00:47:44 +0900

    react-draggableで付箋をドラッグできるように実装

--- frontend/src/app/app/page.tsx
+++ frontend/src/app/app/page.tsx
@@ -1,12 +1,34 @@
+"use client";
 import Husen from "@/components/Husen/Normal";
 import { HUSEN_BLUE_COLOR, HUSEN_GREEN_COLOR ,HUSEN_RED_COLOR} from "@/const";
+import { RefObject, useRef } from "react";
+import Draggable from "react-draggable";
 
-export default function Home() {
+export const Home=()=> {
+  const blueRef: RefObject<HTMLDivElement>  = useRef<HTMLDivElement>(null!);
+  const greenRef: RefObject<HTMLDivElement> = useRef<HTMLDivElement>(null!);
+  const redRef: RefObject<HTMLDivElement> = useRef<HTMLDivElement>(null!);
   return (
+
     <div>
-      <Husen bgColor={HUSEN_BLUE_COLOR} content={"青の付箋"} />
-      <Husen bgColor={HUSEN_GREEN_COLOR} content={"緑の付箋"} />
-      <Husen bgColor={HUSEN_RED_COLOR} content={"赤の付箋"} />
+    <Draggable nodeRef={blueRef}>
+        <div ref={blueRef} className="block w-fit h-fit">
+            <Husen bgColor={HUSEN_BLUE_COLOR} content={"青の付箋"} />
+        </div>
+    </Draggable>
+    <Draggable nodeRef={greenRef}>
+      <div ref={greenRef} className="block w-fit h-fit">
+          <Husen bgColor={HUSEN_GREEN_COLOR} content={"緑の付箋"} />
+      </div>
+    </Draggable>
+    <Draggable nodeRef={redRef}>
+      <div ref={redRef} className="block w-fit h-fit">
+
+          <Husen bgColor={HUSEN_RED_COLOR} content={"赤の付箋"} />
+      </div>
+    </Draggable>
+
     </div>
   );
 }
+export default Home;
